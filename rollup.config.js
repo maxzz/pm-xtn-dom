6426b81645e6c1b8c2d58f2149a9b611b4a8b18f
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-25 18:12:51 -0700

    WIP. add tsconfig for vdom builder

--- rollup.config.js
+++ rollup.config.js
@@ -7,28 +7,52 @@ import typescript from '@rollup/plugin-typescript';
 // (e.g. "rollup -c rollup.config.js --environment target:es5,format:umd,minify",
 const {
     minify,             // true: minify the bundle, false|undefined: do not minify the bundle (default)
-    target,             // target syntax (es5, es6, ...). Default: es5
     format,             // bundle format (umd, cjs, ...). Default: umd
-    npm_package_globalObject,
 } = {
     target: "es2020",
     format: "es",
     ...process.env,
 };
 
-export default {
-    input: `src/index.ts`,
-    output: {
-        format,
-        extend: true,
-        name: npm_package_globalObject,
-        file: `dist/${target}/index${minify ? '.min' : ''}.mjs`,
-        sourcemap: true,
-    },
-    plugins: [
+const minAppendix = minify ? '.min' : '';
+
+function commonPlugins() {
+    return [
         resolve(),
         commonjs(),
         typescript({ declaration: true }),
         (minify ? terser() : [])
-    ]
-};
+    ];
+}
+
+function createConfing_es({ input, output }) {
+    return {
+        input,
+        output: { file: output, format: "es", },
+        plugins: [
+            ...commonPlugins,
+            typescript({ compilerOptions: { declaration: true, outDir: './es6ts', rootDir: './src/vdom-builder' }, tsconfig: 'tsconfig-builder.json' }),
+        ],
+    };
+}
+
+function createConfing_def({ input, output }) {
+    return {
+        input,
+        output: {
+            format,
+            extend: true,
+            file: output,
+            sourcemap: true,
+        },
+        plugins: [
+            ...commonPlugins(),
+        ]
+    };
+}
+
+export default [
+    // createConfing_es({ input: '', output: '' }),
+    //createConfing_def({ input: `src/index.ts`, output: `dist/es6/dom/index${minAppendix}.mjs` }),
+    createConfing_def({ input: 'src/vdom-builder/vdom-server-language.ts', output: `dist/es6/builder/index${minAppendix}.mjs` }),
+];
