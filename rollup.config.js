868b44b235191df9c70a2e6c5133daacd2762678
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-30 16:56:27 -0700

    WIP. done with rollup-plugin-replace-regex

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import replace from '@rollup/plugin-replace';
-import replaceRegex from 'rollup-plugin-replace-regex';
-
 
 // This configuration expects environment parameters passed to the rollup:
 // (e.g. "rollup -c rollup.config.js --environment target:es5,format:umd,minify",
@@ -21,44 +18,9 @@ const minAppendix = minify ? '.min' : '';
 
 function commonPlugins() {
     return [
-        replaceComments(),
         resolve(),
         commonjs(),
     ];
-}
-
-function replaceComments() {
-    console.log('----------------------- replaceComments()');
-    return replaceRegex({
-        preventAssignment: true,
-        // values: {
-        //     '(\/\*\{\}\*\/)': '//',
-        //     'q.q': 'aa',
-        // },
-        // delimiters: ['',''],
-        // values: {
-        //     // 'never now': '"never how"',
-        //     // '/*{}*/': '//',
-        //     // '\/\*\{\}\*\/': '//',
-        // },
-        // regexValues: {
-        //     'q.q': 'qq',
-        //     'k.k': 'kk',
-        //     'a.a': 'aa',
-
-        //     // '\\/\\*\\{\\}\\*\\/': '//',
-        //     '\\/\\*\\{\\}\\*\\/': (id, match, key) => { console.log('regex replace', { id, match, key }); return '//'; },
-        //     '\\/\\*\\[.*\\]\\{\\}\\*\\/': (id, match, key) => { console.log('regex replace', { id, match, key }); return '//'; },
-
-        //     // '\/\*\{\}\*\/': '//',
-        //     // '\\/\\*\\{\\}\\*\\/': '//',
-        //     // '/*{}*/': '//',
-        // },
-        verbose: true,
-        comments: true,
-        conditions: ['all'],
-        commentsForRelease: true,
-    });
 }
 
 function createConfing_es({ input, output }) {
@@ -66,7 +28,7 @@ function createConfing_es({ input, output }) {
         input,
         output: { file: output, format: "es", },
         plugins: [
-            ...commonPlugins,
+            ...commonPlugins(),
             // typescript({ compilerOptions: { declaration: true, outDir: './es6ts', rootDir: './src/vdom-builder' }, tsconfig: 'tsconfig-builder.json' }),
             typescript({ tsconfig: './tsconfig-builder.json' }),
             (minify ? terser() : [])
@@ -94,10 +56,9 @@ function createConfing_def({ input, output, tsconfig }) {
 /** @type import('rollup').RollupOptions[] */
 export default [
     // createConfing_es({ input: '', output: '' }),
-    //createConfing_def({ input: `src/index.ts`, output: `dist/es6/dom/index${minAppendix}.mjs` }),
-    // createConfing_def({ input: 'src/vdom-builder/vdom-server-language.ts', output: `dist/es6/builder/index${minAppendix}.mjs` }),
-
+    createConfing_def({ input: `src/index.ts`, output: `dist/es6/dom/index${minAppendix}.mjs`, tsconfig: './tsconfig.json' }),
+    createConfing_def({ input: 'src/vdom-builder/index.ts', output: `dist/es6/builder/index${minAppendix}.mjs`, tsconfig: './tsconfig-builder.json' }),
 
     // createConfing_def({ input: 'src/vdom-builder/index.ts', output: `dist/index${minAppendix}.mjs`, tsconfig: './tsconfig-builder.json' }),
-    createConfing_def({ input: 'src/utils/format-with.ts', output: `dist/index${minAppendix}.mjs`, tsconfig: './tsconfig.json' }),
+    // createConfing_def({ input: 'src/utils/format-with.ts', output: `dist/index${minAppendix}.mjs`, tsconfig: './tsconfig.json' }),
 ];
